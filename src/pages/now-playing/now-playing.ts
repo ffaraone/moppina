820d1da14842159deb10d95da8c03284e1129db4
Francesco Faraone <ffaraone@gmail.com>
2018-06-21 16:40:21 +0200

    wip background

--- src/pages/now-playing/now-playing.ts
+++ src/pages/now-playing/now-playing.ts
@@ -15,6 +15,8 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class NowPlayingPage {
 
+  backgroundImage: string = '../../assets/imgs/background2.jpg';
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
