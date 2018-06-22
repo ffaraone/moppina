2c0bab3ef75474537eb070486bec04a099cc6082
Francesco Faraone <ffaraone@gmail.com>
2018-06-22 15:38:56 +0200

    slider controls

--- src/pages/now-playing/now-playing.ts
+++ src/pages/now-playing/now-playing.ts
@@ -24,6 +24,8 @@ export class NowPlayingPage {
   trackSeekPos: number = 0;
   trackName: string = '';
 
+  currentState: string = '';
+
   volume: number = 0;
   //btnPlayEnabled: boolean = false;
 
@@ -31,9 +33,11 @@ export class NowPlayingPage {
     this.mopidy = new Mopidy({
       webSocketUrl: 'ws://mappina.velasuci.com:6680/mopidy/ws/'
     });
-    this.mopidy.on(console.log.bind(console));
+    //this.mopidy.on(console.log.bind(console));
     this.mopidy.on('state:online', () => {
+      this.mopidy.mixer.getVolume().then(vol => this.volume = vol);
       this.mopidy.playback.getState().then((state) => {
+        this.currentState = state;
         this.playPauseIcon = state === 'playing' ? 'pause': 'play';
       });
       this.mopidy.playback.getCurrentTrack().then((track) => {
@@ -48,9 +52,8 @@ export class NowPlayingPage {
         this.trackName = track.name;
           setInterval(() => {
             this.mopidy.playback.getTimePosition().then((val) => {
-              console.log(val);
               this.trackSeekPos = val;
-            })
+            });
           }, 1000);
           this.configureEventHandlers();
       })
@@ -67,12 +70,36 @@ export class NowPlayingPage {
 
   }
 
+  playPause() {
+    if (this.currentState === 'playing') {
+      this.mopidy.playback.pause();
+    } else {
+      this.mopidy.playback.play();
+    }
+  }
+  next() {
+    this.mopidy.playback.next();
+  }
+  previous() {
+    this.mopidy.playback.previous();
+  }
 
   configureEventHandlers() {
-    this.mopidy.on('track_playback_started', (track) => {
-      console.log('track playback started: ' + JSON.stringify(track, null, 4));
+    // this.mopidy.on('track_playback_started', (track) => {
+    //   console.log('track playback started: ' + JSON.stringify(track, null, 4));
+    // });
+    this.mopidy.on('event:trackPlaybackStarted', (data) => {
+      if (data.tl_track) {
+        this.trackLength = data.tl_track.track.length;
+        this.trackName = data.tl_track.track.name;
+      }
+    });
+    this.mopidy.on('event:volumeChanged', (vol) => {
+      this.volume = vol.volume;
     });
     this.mopidy.on('event:playbackStateChanged', (state) => {
+      console.log('playback state changed ' + state);
+      this.currentState = state.new_state;
       if (state.new_state === 'playing') {
         this.playPauseIcon = 'pause';
       } else {
@@ -95,5 +122,12 @@ export class NowPlayingPage {
   ionViewDidLoad() {
     console.log('ionViewDidLoad NowPlayingPage');
   }
-
+  changeVolume() {
+    console.log('changing volume');
+    this.mopidy.mixer.setVolume(this.volume);
+  }
+  seek() {
+    console.log('seek');
+    this.mopidy.playback.seek(this.trackSeekPos);
+  }
 }
